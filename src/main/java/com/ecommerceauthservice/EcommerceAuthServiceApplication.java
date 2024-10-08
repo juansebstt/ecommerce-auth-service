89d62a4f4adedbeff8ac5cada3b2e2ee5056893d
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-08 12:02:35 -0500

    first commit

---
+++ src/main/java/com/ecommerceauthservice/EcommerceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceAuthServiceApplication.class, args);
+    }
+
+}
